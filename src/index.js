eb656def178c51c2c70d203ee15926b276ed5a6b
Alex Mena <alexkamu.m@gmail.com>
2022-02-15 12:30:46 -0400

    Initial Commit: React configuration
    Instalación de dependencias para React:
    - Webpack
    - Babel
    - React, ReactDOM y React-Router
    - Loaders de HTML y CSS
    - Eslint y Prettier

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import Dog from './assets/images/work.jpeg'
+import './styles/index.css'
+
+ReactDOM.render(
+    <div>
+        <h1>A TRABAJAR</h1>
+        <img src={Dog} />
+    </div>,
+    document.getElementById('root')
+);
