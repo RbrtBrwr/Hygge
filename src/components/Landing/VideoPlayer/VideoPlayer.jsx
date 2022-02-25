10917f1cff559cf57cb29f297e997daebcaec50f
Alex Mena <alexkamu.m@gmail.com>
2022-02-25 13:47:46 -0400

    VideoPlayer error and Hover animations added

--- src/components/Landing/VideoPlayer/VideoPlayer.jsx
+++ src/components/Landing/VideoPlayer/VideoPlayer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import beachVideo from '../../../assets/videos/Beach.mp4'
+import forestVideo from '../../../assets/videos/Forest.mp4'
 import smallLogo from '../../../assets/logos/logo-small-white.png'
 import './VideoPlayer.css'
 
@@ -9,10 +9,10 @@ const VideoPlayer = () => {
             <figure className='Video-player-logo'>
                 <img src={smallLogo} alt="Hygge's Logo" />
             </figure>
-            <video className='Home-video' id='Home-video' autoPlay={true} muted>
-                <source src={beachVideo} />
+            <video className='Home-video' id='Home-video' loop autoPlay={true} muted>
+                <source src={forestVideo} />
             </video>
-            <p>Ingresa y encuentra que aventuras te deparan con nosotros</p>
+            <p>Ingresa y encuentra qué aventuras te deparan con nosotros</p>
         </div>
     )
 }
